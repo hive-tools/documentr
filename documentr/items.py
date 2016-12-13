04affd2270a84410915150b0ad3fc56ea00efe5d
ssola <introduccio@gmail.com>
2016-12-13 01:09:31 +0100

    move classes

---
+++ documentr/items.py
@@ -0,0 +1,63 @@
+class Table(object):
+    def __init__(self, database, table):
+        self.__database = database
+        self.__table = table
+
+    @property
+    def database(self):
+        return self.__database
+
+    @property
+    def table(self):
+        return self.__table
+
+
+class TableMetadata(object):
+    def __init__(self, metadata):
+        self.__author = metadata['author'] if 'author' in metadata else None
+        self.__version = metadata['version'] if 'version' in metadata else None
+        self.__description = metadata['description'] if 'description' in metadata else None
+
+    @property
+    def author(self):
+        return self.__author
+
+    @property
+    def version(self):
+        return self.__version
+
+    @property
+    def description(self):
+        return self.__description
+
+
+class FieldType(object):
+    def __init__(self, name, field_type, has_comment, comment, field_metadata):
+        self.__name = name
+        self.__field_type = field_type
+        self.__has_comment = has_comment
+        self.__comment = comment
+        self.__metadata = field_metadata
+
+    @property
+    def field_name(self):
+        return self.__name.lower()
+
+    @property
+    def field_type(self):
+        return self.__field_type.upper()
+
+    @property
+    def field_has_comment(self):
+        if self.__has_comment.lower() == "comment":
+            return True
+
+        return False
+
+    @property
+    def field_comment(self):
+        return self.__comment
+
+    @property
+    def field_metadata(self):
+        return self.__metadata
---
+++ documentr/parser.py
@@ -0,0 +1,90 @@
+import re
+from items import TableMetadata, Table, FieldType
+from abc import ABCMeta, abstractmethod
+
+
+class Parser:
+    def __init__(self):
+        pass
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def parse_fields(self, sql):
+        raise Exception("Method not implemented")
+
+    @abstractmethod
+    def parse_table(self, sql):
+        raise Exception("Method not implemented")
+
+    @abstractmethod
+    def parse_table_metadata(self, sql):
+        raise Exception("Method not implemented")
+
+
+class HiveTableParser(Parser):
+    FIELDS_REGEX = "([a-zA-Z_]+)\s+(STRING|BIGINT|INT|DOUBLE|string|bigint|int|double)\s?(COMMENT\s)?(\".*\")?"
+    TABLE_REGEX = "[EXISTS|TABLE|table|exists][\s]+([a-zA-Z0-9_`?]+\.[" \
+                "a-z-A-Z0-9_`?]+)"
+    TABLE_METADATA_REGEX = "@(author|description|version)\(\"([\w\s\d,\-\.\'_]+)\"\)"
+    FIELD_METADATA = "@(reference)\(([\w\s\d,\-\.\'_]+)\)"
+
+    def __init__(self):
+        super(HiveTableParser, self).__init__()
+
+        self.table = None
+        self.fields = []
+        self.table_metadata = None
+
+    def parse_table(self, sql):
+        items = re.findall(self.TABLE_REGEX, sql)
+        if len(items) == 0:
+            raise ValueError("No table or database found")
+
+        items = items[0].split('.')
+
+        self.table = Table(items[0], items[1])
+
+        return self.table
+
+    def parse_fields(self, sql):
+        _fields = re.findall(self.FIELDS_REGEX, sql, re.MULTILINE)
+
+        if not _fields:
+            pass
+
+        # Get from regular fields
+        for field in _fields:
+            if len(field) == 4:
+                _metadata = None
+                if field[3]:
+                    _metadata = self.parse_field_metadata(field[3])
+                self.fields.append(
+                    FieldType(field[0], field[1], field[2], field[3], _metadata)
+                )
+
+        return self.fields
+
+    def parse_table_metadata(self, sql):
+        items = re.findall(self.TABLE_METADATA_REGEX, sql)
+        allowed_items = ['author', 'version', 'description']
+        stored_items = {}
+
+        for value in items:
+            if value[0] in allowed_items:
+                stored_items[value[0]] = value[1]
+
+        self.table_metadata = TableMetadata(stored_items)
+
+        return self.table_metadata
+
+    def parse_field_metadata(self, comment):
+        items = re.findall(self.FIELD_METADATA, comment)
+        allowed_itmes = ['reference']
+        stored_items = {}
+
+        for value in items:
+            if value[0] in allowed_itmes:
+                stored_items[value[0]] = value[1]
+
+        return stored_items
