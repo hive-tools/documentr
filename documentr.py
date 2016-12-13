1f54fb92c42d62b259add0348d820f68081ff5d5
ssola <introduccio@gmail.com>
2016-12-13 01:42:21 +0100

    update example

--- documentr.py
+++ documentr.py
@@ -5,36 +5,16 @@ from documentr.writer import Writer
 sql = """
 /**
  * @author("Sergio Sola")
- * @description("This table creates some stuff")
+ * @description("This table creates a fact table with active customers")
  * @version("1.0.0")
  */
-CREATE EXTERNAL TABLE IF NOT EXISTS fact_tables.errors_reported (
-    mongo_object_id  STRING COMMENT "@reference(database.mongo_object_id)THIS IS A COMMENT an aweful comment",
-    fk_subscription BIGINT,
-    fk_customer BIGINT,
-    fk_entered_date INT,
-    fk_product BIGINT,
-    fk_delivery_schedule INT,
-    country STRING,
-    agent STRING,
-    box_id STRING,
-    hellofresh_week_where_error_happened STRING,
-    channel STRING,
-    compensation_type STRING,
-    compensation_amount DOUBLE,
-    customer_emotion INT,
-    description_total STRING,
-    reason STRING,
-    subreason STRING,
-    ingredient STRING,
-    error_description STRING,
-    has_sorry_gift INT,
-    is_pr_box INT,
-    inserted_at BIGINT
+CREATE EXTERNAL TABLE IF NOT EXISTS fact_tables.active_customers (
+    customer_id  BIGINT COMMENT "@reference(dimensions.customers.customer_sk) Reference to customer in time",
+    product STRING COMMENT "@reference(other_tables.product.sku) Stores the product SKU"
  )
- PARTITIONED BY (hellofresh_week string, country string)
+ PARTITIONED BY (country string)
 STORED AS PARQUET
-LOCATION '/HelloFresh/Databases/fact_tables/errors_reported';
+LOCATION '/YourCompany/fact_tables/active_customers';
 """
 
 parser = HiveTableParser()
