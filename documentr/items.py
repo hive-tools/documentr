f4e44d95e82b66bb05eda862941681243fa59c98
Sergio Sola <introduccio@gmail.com>
2017-01-22 01:12:16 +0100

    start writing some tests

--- documentr/items.py
+++ documentr/items.py
@@ -1,10 +1,16 @@
 import re
 
 
+class Cleaner(object):
+    @staticmethod
+    def clean_str(str):
+        return str.replace('`', '')
+
+
 class Table(object):
     def __init__(self, database, table):
-        self.__database = database
-        self.__table = table
+        self.__database = Cleaner.clean_str(database)
+        self.__table = Cleaner.clean_str(table)
 
     @property
     def database(self):
@@ -70,3 +76,16 @@ class FieldType(object):
     @property
     def field_metadata(self):
         return self.__metadata
+
+    def __eq__(self, other):
+        _NOTFOUND = object()
+
+        for attr in ['field_name', 'field_type', 'field_comment',
+                     'field_metadata']:
+            v1, v2 = [getattr(obj, attr, _NOTFOUND) for obj in [self, other]]
+            if v1 is _NOTFOUND or v2 is _NOTFOUND:
+                return False
+            elif v1 != v2:
+                return False
+
+        return True
---
+++ tests/documentr/unit/parser_test.py
@@ -0,0 +1,47 @@
+import unittest2
+
+from documentr.parser import ParserFactory
+from documentr.items import FieldType
+
+
+class HiveTableParserTest(unittest2.TestCase):
+    def setUp(self):
+        self.sql = """
+            /**
+             * @author('Sergio Sola')
+             * @version('v1.0.1')
+             * @description('This table is used to store some data')
+             */
+            CREATE EXTERNAL TABLE IF NOT EXISTS `fact_tables`.some_table (
+                country STRING COMMENT "Country",
+                fk_date INT COMMENT "Foreing key to Date dimension",
+                campaign STRING COMMENT "Campaing ID",
+                subchannel STRING COMMENT "Sub channel used for some reason"
+             )
+            PARTITIONED BY (
+              week STRING COMMENT "To which week it belongs"
+            )
+            STORED AS PARQUET
+            LOCATION '/HelloFresh/Databases/fact_tables/some_table';
+        """
+
+        self.hive_parser = ParserFactory.create_parser("hive")
+
+    def test_parse_table(self):
+        table = self.hive_parser.parse_table(self.sql)
+
+        self.assertEqual("fact_tables", table.database)
+        self.assertEqual("some_table", table.table)
+
+    def test_parse_fields(self):
+        expected_value = [
+            FieldType('country', 'STRING', True, "Country", {}),
+            FieldType('fk_date', 'INT', False, "Foreing key to Date dimension", {}),
+            FieldType('campaign', 'STRING', False, "Campaing ID", {}),
+            FieldType('subchannel', 'STRING', False, "Sub channel used for some reason", {}),
+            FieldType('week', 'STRING', False, "To which week it belongs", {}),
+        ]
+
+        fields = self.hive_parser.parse_fields(self.sql)
+
+        self.assertItemsEqual(expected_value, fields)
