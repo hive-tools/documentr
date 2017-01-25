ce8b1d6735d0d01f9d4a84a15ef2274b5ef895ad
Sergio Sola <introduccio@gmail.com>
2017-01-25 19:03:41 +0100

    creating infra stuff

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 from setuptools import find_packages
-
+from pip.req import parse_requirements
 
 NAME = "documentr"
 VERSION = "1.0.0"
 
-REQUIRES = [
-    "Jinja2"
+try:
+    install_reqs = parse_requirements('requirements.txt', session=False)
+    requirements = [str(ir.req) for ir in install_reqs]
+except OSError:
+    requirements = []
+
+test_requirements = [
+    'pytest'
 ]
 
 setup(
@@ -15,7 +21,8 @@ setup(
     description="Documentor",
     author_email="introduccio@gmail.com",
     keywords=["hive", "documentation"],
-    install_requires=REQUIRES,
-    tests_require=[],
-    packages=find_packages()
+    install_requires=requirements,
+    tests_require=test_requirements,
+    packages=find_packages(),
+    tests_suite='tests',
 )
